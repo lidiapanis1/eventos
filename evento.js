fb7dda77137782ed7db64c853361905f901a96b2
lidiapanis1 <89846034+lidiapanis1@users.noreply.github.com>
2021-09-28 21:17:55 -0300

    Add files via upload

---
+++ evento.js
@@ -0,0 +1,30 @@
+const diaevento = 30 ;
+const mesevento = 10;
+const anoevento = 2021;
+const diahoje = 28;
+const meshoje = 09;
+const anohoje= 2021;
+const idade = 19;
+var index = 0;
+
+if (meshoje <= mesevento) {
+    if (diahoje <= diaevento){
+        console.log ('Data do evento: '+ diaevento + mesevento + anoevento);
+    } else{
+        console.log ('Cadastro não permitido');
+        end
+    }
+}
+if (idade >=18){
+    console.log ('Você tem permissão');
+    console.log ('Idade do participante é' +idade);
+} else {
+    console.log ('Cadastro não permitido pela idade');
+    end
+}
+
+console.log ('Participante numero' +index);
+index = index++;
+end
+
+
